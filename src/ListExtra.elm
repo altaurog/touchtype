5a50772d7b60e8ea4cbd536a3d1a36e80385b27f
Aryeh Leib Taurog <git@aryehleib.com>
2018-04-05 12:50:23 +0300

    refactor: separate zip and use composition

--- src/ListExtra.elm
+++ src/ListExtra.elm
@@ -2,9 +2,9 @@ module ListExtra exposing (..)
 
 
 leftZip : List a -> List b -> List (a, Maybe b)
-leftZip xs ys =
-  pad (List.length xs) ys
-    |> List.map2 (,) xs
+leftZip xs =
+  pad (List.length xs)
+    >> zip xs
 
 
 pad : Int -> List a -> List (Maybe a)
@@ -14,3 +14,7 @@ pad len xs =
     padding = if 0 < d then List.repeat d Nothing else []
   in
     (List.map Just xs) ++ padding
+
+
+zip : List a -> List b -> List (a, b)
+zip = List.map2 (,)
