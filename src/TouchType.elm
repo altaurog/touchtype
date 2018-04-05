8fe6faff1dbdca6cc783b23ce2a5aa4d775f97a6
Aryeh Leib Taurog <git@aryehleib.com>
2018-04-05 15:29:06 +0300

    feat: touchtype business logic

---
+++ src/TouchType.elm
@@ -0,0 +1,33 @@
+module TouchType exposing (..)
+
+{-| This module provides business logic for touchtype app
+-}
+
+import ListExtra
+
+
+{-| A checked input is either ok or erroneous.
+We reappropriate `Result` for this purpose;
+Ok and Err values are both the input character.
+-}
+type alias Checked = Result Char Char
+
+
+{-| Check a line of input against its master
+-}
+checkLine : (String, Maybe String) -> List Checked
+checkLine (master, input) =
+  let
+    i = Maybe.withDefault "" input
+  in
+    ListExtra.rightZip (String.toList master) (String.toList i)
+      |> List.map checkChar
+
+
+{-| Check a (master, input) character pair.
+-}
+checkChar : (Maybe Char, Char) -> Checked
+checkChar pair =
+  case pair of
+    (Just m, i) -> if i == m then Ok i else Err i
+    (Nothing, i) -> Err i
---
+++ tests/TestTouchType.elm
@@ -0,0 +1,54 @@
+module TestTouchType exposing (suite)
+
+import Expect
+import Test exposing (..)
+
+import TouchType exposing (..)
+
+suite : Test
+suite =
+  describe "Touchtype functions"
+    [ describe "checkChar"
+      [ test "checkChar correct char" <|
+          \_ ->
+            TouchType.checkChar (Just 'a', 'a')
+              |> Expect.equal (Ok 'a')
+
+      , test "checkChar incorrect char" <|
+          \_ ->
+            TouchType.checkChar (Just 'b', 'a')
+              |> Expect.equal (Err 'a')
+
+      , test "checkChar extra char" <|
+          \_ ->
+            TouchType.checkChar (Nothing, 'a')
+              |> Expect.equal (Err 'a')
+      ]
+
+    , describe "checkLine"
+      [ test "checkLine correct and complete" <|
+          \_ ->
+            TouchType.checkLine ("The ", Just "The ")
+              |> Expect.equal [(Ok 'T'), (Ok 'h'), (Ok 'e'), (Ok ' ')]
+
+      , test "checkLine correct but incomplete" <|
+          \_ ->
+            TouchType.checkLine ("The ", Just "Th")
+              |> Expect.equal [(Ok 'T'), (Ok 'h')]
+
+      , test "checkLine incorrect but complete" <|
+          \_ ->
+            TouchType.checkLine ("The ", Just "Tha ")
+              |> Expect.equal [(Ok 'T'), (Ok 'h'), (Err 'a'), (Ok ' ')]
+
+      , test "checkLine incorrect and incomplete" <|
+          \_ ->
+            TouchType.checkLine ("The ", Just "Tj")
+              |> Expect.equal [(Ok 'T'), (Err 'j')]
+
+      , test "checkLine empty input" <|
+          \_ ->
+            TouchType.checkLine ("The ", Nothing)
+              |> Expect.equal []
+      ]
+    ]
