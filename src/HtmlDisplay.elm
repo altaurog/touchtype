534f5c2da1750f19c036a364b0a0e55a139be146
Aryeh Leib Taurog <git@aryehleib.com>
2018-04-08 22:40:29 +0300

    feat: display functions

---
+++ src/HtmlDisplay.elm
@@ -0,0 +1,45 @@
+module HtmlDisplay exposing (..)
+
+{-| Display logic for touchtype app
+-}
+
+import Html exposing (Html, Attribute, div, span, text)
+import Html.Attributes exposing (class)
+
+import ListExtra
+import TouchType exposing (Checked, checkLine)
+
+{-| Display an exercise
+-}
+
+exercise : List String -> List String -> Html msg
+exercise master input =
+  ListExtra.leftZip master input
+    |> List.map lineDiv
+    |> div [class "exercise"]
+
+
+{-| Display a master input line pair
+-}
+lineDiv : (String, Maybe String) -> Html msg
+lineDiv (master, input) = div [] [
+    div [] [text master],
+    div [] (checkLine (master, input) |> List.map checkedSpan)
+  ]
+
+
+{-| Display a checked input letter
+-}
+
+checkedSpan : Checked -> Html msg
+checkedSpan checked =
+  case checked of
+    Ok char -> span [class "ok char"] [charText char]
+    Err char -> span [class "error char"] [charText char]
+
+
+{-| Convert a char to Html text element
+-}
+
+charText : Char -> Html msg
+charText c = String.fromList [c] |> text
---
+++ tests/TestHtmlDisplay.elm
@@ -0,0 +1,48 @@
+module TestHtmlDisplay exposing (suite)
+
+import Expect
+import Test exposing (..)
+
+import Html exposing (Html, Attribute, div, span, text)
+import Html.Attributes exposing (class)
+
+import HtmlDisplay exposing (..)
+
+suite : Test
+suite =
+  describe "HTML Display functions"
+    [ describe "charText"
+      [ test "convert char to HTML text element" <|
+          \_ ->
+            HtmlDisplay.charText 'a'
+              |> Expect.equal (text "a")
+      ]
+
+    , describe "checkedSpan"
+      [ test "convert correct char to span" <|
+          \_ ->
+            HtmlDisplay.checkedSpan (Ok 'a')
+              |> Expect.equal (span [class "ok char"] [text "a"])
+
+      , test "convert incorrect char to span" <|
+          \_ ->
+            HtmlDisplay.checkedSpan (Err 'a')
+              |> Expect.equal (span [class "error char"] [text "a"])
+      ]
+
+    , describe "lineDiv"
+      [ test "convert master/input text to html" <|
+          \_ ->
+            HtmlDisplay.lineDiv ("I am", Just "I mm")
+              |> Expect.equal (div [] [
+                  div [] [text "I am"],
+                  div [] [
+                    span [class "ok char"] [text "I"],
+                    span [class "ok char"] [text " "],
+                    span [class "error char"] [text "m"],
+                    span [class "ok char"] [text "m"]
+                  ]
+                ])
+      ]
+    ]
+
